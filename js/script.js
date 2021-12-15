83c0a5fe4111fb4831920cb4f862a8b44d3362ac
Will160714 <95445527+Will160714@users.noreply.github.com>
2021-12-15 14:10:19 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,66 @@
 console.log("script is loading");
 
-let adventureDescriptionText = document.querySelector(".adventure-description");
-let optionButtons = document.getElementsByClassName("button");
+// select the container of the game description
+const descriptionText = document.querySelector(".adventure-description");
+const optionButtons = document.getElementsByClassName("button");
 
-function updateAdventureText() {
-  adventureDescriptionText.innerText = textAdventure[0].text;
+// function that starts the game
+function startAdventure() {
+  console.log("Adventure has started");
+  showAdventureOptions(1);
 }
 
-function updateOptions() {
-  let i = 0;
-  for(i; i < optionButtons.length; i++) {
-    console.log(optionButtons[i]);
-    optionButtons[i].innerText = textAdventure[0].options[i];
+// create a function that shows the description
+function showAdventureOptions(textAdventureID) {
+  const newDescriptionText = textAdventure.find(gameNode => gameNode.id === textAdventureID);
+  descriptionText.innerText = newDescriptionText.description;
+  
+ // iterate through the buttons and insert the option text in each button
+  for(let i = 0; i < newDescriptionText.options.length; i++) {
+ //console.log(newDescriptionText.options[i].buttontext);
+    optionButtons[i].innerText = newDescriptionText.options[i].buttontext;
   }
 }
 
+// function that tracks options and passes the id to the showAdventureOptions function
+
+
+// optional: function that shows end of the game
+
+
+// create and object that holds the text for our game and options and also tracks state
 const textAdventure = [
   {
     id: 1,
-    text: 'Deep within a cave, a man woke up. The man only knew that his name was Percy, but he was clueless regarding his current location. Help Percy survive the unknown and reach human civilizaiton. Choose one of the options below',
-    options: ["Head south and find the river" , "Go back to the bottom of the hill" ]
+    description: 'Deep within a cave, a man woke up. The man only knew that his name was Percy, but he was clueless regarding his current location. Help Percy survive the unknown and reach human civilizaiton. Choose one of the options below',
+    options: [
+      {
+        buttontext: "Move north",
+        setState: { headback: true },
+        nexttext: 2
+      },
+      {
+        buttontext: "Move west",
+        nextText: 2       
+      }
+    ]
   },
-]
+  {
+    id: 2,
+    description: 'Percy, discovers a river, but it is almost night time. What should Percy do now?',
+    options: [
+      {
+        buttontext: "Follow the river down south",
+        setState: { headback: true },
+        nexttext: 3
+      },
+      {
+        buttontext: "Return back to the cave and wait for the next day",
+        nextText: 2       
+      }
+    ]
+  },
+  
+];
 
-updateAdventureText();
-updateOptions();
+startAdventure();
