1fdc49eaa5c8ec738c131fe98185e727d067edce
Will160714 <95445527+Will160714@users.noreply.github.com>
2021-12-15 14:18:14 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,66 +1,132 @@
 console.log("script is loading");
 
-// select the container of the game description
 const descriptionText = document.querySelector(".adventure-description");
 const optionButtons = document.getElementsByClassName("button");
 
-// function that starts the game
-function startAdventure() {
+let startIndex = 0;
+
+function startAdventure(textAdventureIndex) {
   console.log("Adventure has started");
-  showAdventureOptions(1);
+  showAdventureOptions(textAdventureIndex);
 }
 
-// create a function that shows the description
 function showAdventureOptions(textAdventureID) {
-  const newDescriptionText = textAdventure.find(gameNode => gameNode.id === textAdventureID);
+  // check fisrt
+
+  const newDescriptionText = textAdventure.find(
+    (gameNode) => gameNode.id === textAdventureID
+  );
+  if (!newDescriptionText.options) {
+    document.getElementById("first_btn").style.display = "none";
+
+    descriptionText.innerHTML = newDescriptionText.message;
+
+    document.getElementById("second_btn").innerHTML = "Play Again";
+    return;
+  }
+
   descriptionText.innerText = newDescriptionText.description;
-  
- // iterate through the buttons and insert the option text in each button
-  for(let i = 0; i < newDescriptionText.options.length; i++) {
- //console.log(newDescriptionText.options[i].buttontext);
+  for (let i = 0; i < newDescriptionText.options.length; i++) {
     optionButtons[i].innerText = newDescriptionText.options[i].buttontext;
   }
 }
 
-// function that tracks options and passes the id to the showAdventureOptions function
+function buttonOptions() {
+  const adventureIndex = textAdventure.find(
+    (gameNode) => gameNode.id === startIndex
+  );
+
+  for (let i = 0; i < optionButtons.length; i++) {
+    optionButtons.addEventListener("click", function () {});
+  }
+}
+
+function onPlay() {
+  let ids = textAdventure.map((item) => {
+    return item.id;
+  });
+
+  if (startIndex > ids.length - 1) {
+    startIndex = 0;
+  }
+
+  // show
+  showAdventureOptions(ids[startIndex]);
+}
+
+function onNextOption(event) {
+  event.preventDefault();
+
+  startIndex++;
+
+  onPlay();
+}
+
+function onPlayAgain(event) {
+  event.preventDefault();
+
+  document.getElementById("first_btn").style.display = "inline";
+
+  startIndex = 0;
+
+  onPlay();
+}
 
 
-// optional: function that shows end of the game
-
-
-// create and object that holds the text for our game and options and also tracks state
 const textAdventure = [
   {
     id: 1,
-    description: 'Deep within a cave, a man woke up. The man only knew that his name was Percy, but he was clueless regarding his current location. Help Percy survive the unknown and reach human civilizaiton. Choose one of the options below',
+    description:
+      "Deep within a cave, a man woke up. The man only knew that his name was Percy, but he was clueless regarding his current location. Choose one of the options below",
     options: [
       {
-        buttontext: "Move north",
+        buttontext: "Head north",
         setState: { headback: true },
-        nexttext: 2
+        nexttext: 2,
       },
       {
-        buttontext: "Move west",
-        nextText: 2       
-      }
-    ]
+        buttontext: "Head west.",
+        message: console.log("Try again"),
+      },
+    ],
   },
   {
     id: 2,
-    description: 'Percy, discovers a river, but it is almost night time. What should Percy do now?',
+    description:
+      "Percy finds a river, but it's almost night time, what should Percy do now?",
     options: [
       {
-        buttontext: "Follow the river down south",
+        buttontext: "Follow the river southwards",
         setState: { headback: true },
-        nexttext: 3
+        nexttext: 3,
       },
       {
-        buttontext: "Return back to the cave and wait for the next day",
-        nextText: 2       
-      }
-    ]
+        buttontext: "Head back to the cave.",
+        message: console.log("Try again. Percy became lost and eventually lost his life."),
+      },
+    ],
   },
-  
+  {
+    id: 3,
+    description:
+      "It is now night time and howls could be heard. What should Percy do?",
+    options: [
+      {
+        buttontext: "Continue southwards.",
+        setState: { headback: true },
+      },
+      {
+        buttontext: "Wait until the sun comes out again before heading south again.",
+        setState: { daylight: true },
+        nexttext: 4,
+      },
+    ],
+  },
+  {
+    id: 4,
+    message: "Percy reunites with civilization!",
+  },
 ];
 
-startAdventure();
+
+onPlay();
